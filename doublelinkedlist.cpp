84243a0524f6c53d21fbebf01fa65def86bf39a5
ardiapri <ardiapri882@gmail.com>
2024-05-04 12:13:56 +0700

    menambahkan void addnode

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -10,3 +10,20 @@ struct Node
 };
 
 Node* START = NULL;
+void addNode()
+{
+    Node* newNode = new Node();
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode->noMhs;
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode->name;
+
+    // insert the new node int the list
+    if (START == NULL || newNode->noMhs <= START->noMhs)
+    { // insert the new node at the beginning. 2
+
+        if (START != NULL && newNode->noMhs == START->noMhs)
+        {
+            cout << "\033[31mDuplicate roll numbers not allowed\033]0m" << endl;
+            return;
+        }
