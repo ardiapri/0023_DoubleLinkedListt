ca5cad3cc26910defd187082506d8d91d7e79ad3
ardiapri <ardiapri882@gmail.com>
2024-05-04 12:15:14 +0700

    memasukan folder baru ke dalam daftar

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -27,3 +27,13 @@ void addNode()
             cout << "\033[31mDuplicate roll numbers not allowed\033]0m" << endl;
             return;
         }
+        // if the list is empty, make the new node the START
+        newNode->next = START; // 3. make the new node point to the first node
+        if (START != NULL)
+        {
+            START->prev = newNode; // 4. make the first node point to the new node
+        }
+        newNode->prev = NULL; // 5. make the new node point to NULL
+        START = newNode;      // 6. make the new node the first node
+    }
+
