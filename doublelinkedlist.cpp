1a6e5189596a9010fbcaef506691deea9ff932a6
ardiapri <ardiapri882@gmail.com>
2024-05-04 12:26:27 +0700

    memasukkan roll mahasiswa

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -151,5 +151,12 @@ void searchData()
     {
         cout << "\nList is empty" << endl;
     }
+    Node* prev, * curr;
+    prev = curr = NULL;
+    cout << "\nEnter the roll number of the student whose record you want to search: ";
+    int num;
+    cin >> num;
+    if (search(num, &prev, &curr) == false)
+        cout << "\nRecord not found" << endl;
 
 
